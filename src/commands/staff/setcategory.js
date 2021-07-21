77401ace7e69f2182dd9acd42a0ee68e6170f2aa
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 20:00:46 +0530

    update setcategory.js

--- src/commands/staff/setcategory.js
+++ src/commands/staff/setcategory.js
@@ -5,6 +5,7 @@ module.exports = {
     run : async(client, message, args) => {
     	if (!message.member.roles.cache.has(process.env.staff)) return message.channel.send(":x: You don't have the required permission to run this command")
     const id = args[0]
+    
     if (!id) {
     	const noarg = new MessageEmbed()
     .setColor("RANDOM")
@@ -14,5 +15,31 @@ module.exports = {
     .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
     message.channel.send(noarg)
     }
+    else {
+    	const cy = message.guild.channels.cache.get(id)
+    if (!cy) {
+    	const unknown = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎Unknown Argument")
+    .setDescription("The ID you provided was invalid. Please provide a valid ID.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(unknown)
+    }
+    else if  (cy.type !== "category") {
+    	const invalid = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎Invalid Argument")
+    .setDescription("The ID you provided was of a non-category channel. Please provide a category id.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(invalid)
+    	}
+    else {
+    	
+    }
+    
+    }
+    
     }
 } 

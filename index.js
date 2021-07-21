141ae530f5a9402ca28e4430edccc8a1348347c2
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 22:58:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ const pinger = require("simple-website-pinger")
 pinger.webserver(3000)
 pinger.ping("https://ticketsbot.rilecraft.repl.co:3000")
 
-client.on('clickButton', async (button) => {
-   button.send("h")
+client.on('clickButton', async (button, message) => {
+   message.reply("a")
 });

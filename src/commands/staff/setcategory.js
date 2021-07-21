3f6cdc7145c043bdca74e1e347203c24b89e6708
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 01:01:08 +0530

    update setcategory.js

--- src/commands/staff/setcategory.js
+++ src/commands/staff/setcategory.js
@@ -3,6 +3,7 @@ const db = require('quick.db')
 module.exports = {
     name : 'setcategory',
     run : async(client, message, args) => {
+    	if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) return message.channel.send(":x: Missing Permissions \`Manage Channels\`")
     	if (!message.member.roles.cache.has(process.env.staff)) return message.channel.send(":x: You don't have the required permission to run this command")
     const id = args[0]
     

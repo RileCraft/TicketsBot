51bc90c093523a5aff655447b81f5ce579f82860
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 18:48:07 +0530

    update ping.js

---
+++ src/commands/basic/ping.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+module.exports = {
+    name : 'ping',
+    usage: "s!ping",
+description: "Shows the latency of Bot and the Websocket.",
+    run : async(client, message, args) => {
+        const ping = new MessageEmbed()
+	.setColor('RANDOM')
+	.setTimestamp()
+        .setTitle('🏓╎Pong!')
+	.setDescription('🏠╎Websocket Latency:' + " " + client.ws.ping + "ms" + '\n🤖╎Bot Latency:' + " " + `${Date.now() - message.createdTimestamp}` + "ms");
+	message.channel.send(ping)
+    }
+}

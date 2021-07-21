96bfb85345958b6a26798a5aed790c9caa4f8f64
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 19:29:58 +0530

    update ticket.js

--- src/commands/basic/ticket.js
+++ src/commands/basic/ticket.js
@@ -6,7 +6,7 @@ module.exports = {
     	const name = message.author.username
     message.guild.channels.create("ticket-" + name, {
   type: 'text',
-  parent: "772829053861888050",
+  parent: db.get("ticket.category")
   permissionOverwrites: [
      {
        id: message.author.id,

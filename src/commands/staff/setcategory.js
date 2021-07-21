4141a8ccba3aee6a9ce2e5ac61c6ce664a9f324f
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 19:54:09 +0530

    update setcategory.js

--- src/commands/staff/setcategory.js
+++ src/commands/staff/setcategory.js
@@ -4,6 +4,15 @@ module.exports = {
     name : 'setcategory',
     run : async(client, message, args) => {
     	if (!message.member.roles.cache.has(process.env.staff)) return message.channel.send(":x: You don't have the required permission to run this command")
-    message.reply("a")
+    const id = args[0]
+    if (!id) {
+    	const noarg = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎No Argument")
+    .setDescription()
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(noarg)
+    }
     }
 } 

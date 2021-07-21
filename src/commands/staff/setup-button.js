c9a1d2d75d266b12096f42c5179f3726426f0408
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 22:33:16 +0530

    update setup-button.js

---
+++ src/commands/staff/setup-button.js
@@ -0,0 +1,15 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+const { MessageButton } = require("discord-buttons")
+module.exports = {
+    name : 'setup',
+    run : async(client, message, args) => {
+    	if (!message.member.roles.cache.has(process.env.staff)) return message.channel.send(":x: You don't have the required permission to run this command")
+    let button = new MessageButton()
+  .setStyle('blurple')
+  .setLabel('Open Ticket') 
+  .setID('buton') 
+
+message.channel.send('Interact with the button to create a ticket!', button);
+    }
+} 

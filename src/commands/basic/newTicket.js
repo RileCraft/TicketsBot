8b6950a6a5abb1258967ee111a87917561499865
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 00:58:58 +0530

    update newTicket.js

--- src/commands/basic/newTicket.js
+++ src/commands/basic/newTicket.js
@@ -3,7 +3,7 @@ const db = require('quick.db')
 module.exports = {
     name : 'ticket',
     run : async(client, message, args) => {
-    	if (!message.guild.me.permissions.has("MANAGE_CHANNEL")) return message.channel.send(":x: Missing Permissions \`Manage Channels\`")
+    	if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) return message.channel.send(":x: Missing Permissions \`Manage Channels\`")
     	const name = message.author.username
     message.guild.channels.create("ticket-" + name, {
   type: 'text',

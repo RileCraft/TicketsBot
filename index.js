27e5b11c63be91cc734dffbcde2a450587493bd7
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 22:47:51 +0530

    update index.js

--- index.js
+++ index.js
@@ -57,27 +57,5 @@ pinger.webserver(3000)
 pinger.ping("https://ticketsbot.rilecraft.repl.co:3000")
 
 client.on('clickButton', async (button) => {
-    const db = require('quick.db')
-		const name = `${button.clicker.user.username}`
-    message.guild.channels.create("ticket-" + name, {
-  type: 'text',
-  parent: db.get("ticket.category"),
-  permissionOverwrites: [
-     {
-       id: message.author.id,
-       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
-    },
-     {
-       id: message.guild.roles.everyone.id,
-       deny: ['VIEW_CHANNEL', "SEND_MESSAGES"],
-    },
-    {
-       id: process.env.staff,
-       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
-    },
-  ],
-}).then(channel => {
-	db.set(channel.id + ".ticket", "true")
-	channel.send("<@" + `${button.clicker.user.id}` + ">")
-	})
+    button.reply("hi")
 });

b55b631d89b56a5a51e992ecb78ae36979dd74f9
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 18:57:00 +0530

    update ticket.js

---
+++ src/commands/basic/ticket.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+module.exports = {
+    name : 'ticket',
+    run : async(client, message, args) => {
+    	const name = message.author.username
+    message.guild.createChannel("ticket-" + name, "text").then(channel => {
+    	channel.setParent("772829053861888050")
+    })
+    }
+} 

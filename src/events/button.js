bf5fb0969184e533eddbf66af9df60c02709fec0
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 22:37:09 +0530

    update button.js

---
+++ src/events/button.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'button',
+	execute(button) {
+		const name = `${button.clicker.user.username}`
+    message.guild.channels.create("ticket-" + name, {
+  type: 'text',
+  parent: db.get("ticket.category"),
+  permissionOverwrites: [
+     {
+       id: message.author.id,
+       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+     {
+       id: message.guild.roles.everyone.id,
+       deny: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+    {
+       id: process.env.staff,
+       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+  ],
+}).then(channel => {
+	db.set(channel.id + ".ticket", "true")
+	channel.send("<@" + `${button.clicker.user.id}` + ">")
+	})
+	},
+};

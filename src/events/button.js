db6c56bfc5eace5f16293b1f8984d8001fe7b4de
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-21 23:07:28 +0530

    update button.js

--- src/events/button.js
+++ src/events/button.js
@@ -23,6 +23,7 @@ module.exports = {
   ],
 }).then(channel => {
 	db.set(channel.id + ".ticket", "true")
+	button.reply.send("Here is your ticket: <#" + channel.id + ">")
 	channel.send("<@" + button.clicker.user.id + ">, Here is your ticket!")
 	})
 	} catch (error) {

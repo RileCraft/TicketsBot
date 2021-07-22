9f7ab650448cb9576c152a70e25830c21bb1c184
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 10:36:01 +0530

    update lock-ticket-button.js

--- src/events/lock-ticket-button.js
+++ src/events/lock-ticket-button.js
@@ -2,7 +2,28 @@ module.exports = {
 	name: 'clickButton',
 	execute(button, channel) {
 		if (button.id === "lock") {
-			button.reply.send(button.channel.id)
+			if (!button.guild.me.permissions.has("MANAGE_CHANNELS")) return button.reply.send(":x: Missing Permissions \`Manage Channels\`")
+			const db = require('quick.db')
+			const id = button.channel.id
+			const author = db.get(id + ".author")
+			
+			button.channel.overwritePermissions([
+     {
+       id: button.clicker.user.id,
+       deny: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+     {
+       id: button.guild.roles.everyone.id,
+       deny: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+    {
+       id: process.env.staff,
+       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+  ],
+  )
+  button.reply.send("You have now locked this ticket to be avaliable to staff only!")
+			
 			}
 		},
 };

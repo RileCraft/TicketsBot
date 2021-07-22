af5e5ee0b87c591201cb93ea3a70f3644836aa82
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 11:49:57 +0530

    update newTicket.js

--- src/commands/basic/newTicket.js
+++ src/commands/basic/newTicket.js
@@ -5,6 +5,42 @@ module.exports = {
     run : async(client, message, args) => {
     	if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) return message.channel.send(":x: Missing Permissions \`Manage Channels\`")
     	const name = message.author.username
+    if (!process.env.staff) {
+    	message.guild.channels.create("ticket-" + name, {
+  type: 'text',
+  parent: db.get("ticket.category"),
+  permissionOverwrites: [
+     {
+       id: message.author.id,
+       allow: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+     {
+       id: message.guild.roles.everyone.id,
+       deny: ['VIEW_CHANNEL', "SEND_MESSAGES"],
+    },
+  ],
+}).then(channel => {
+	db.set(channel.id + ".ticket", "true")
+	db.set(channel.id + ".author", message.author.id)
+	message.channel.send("Here is your ticket: <#" + channel.id + ">")
+	const { MessageButton, MessageActionRow } = require('discord-buttons')
+let lock = new MessageButton()
+  .setStyle('red')
+  .setLabel('🔒 Lock this ticket.') 
+  .setID('lock') 
+
+let close = new MessageButton()
+  .setStyle('blurple')
+  .setLabel('🔑 Close this ticket.') 
+  .setID('close')
+
+let buttons = new MessageActionRow()
+  .addComponents(lock, close);
+	
+	channel.send("Ticket Control Panel", buttons)
+	})
+    }
+    else {
     message.guild.channels.create("ticket-" + name, {
   type: 'text',
   parent: db.get("ticket.category"),
@@ -42,5 +78,6 @@ let buttons = new MessageActionRow()
 	
 	channel.send("Ticket Control Panel", buttons)
 	})
+	}
     }
-} 
+}

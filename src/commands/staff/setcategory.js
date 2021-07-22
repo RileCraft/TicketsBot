405b219cb5a344fa82cc49139807f6bb7dcfadc4
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 10:59:20 +0530

    update setcategory.js

--- src/commands/staff/setcategory.js
+++ src/commands/staff/setcategory.js
@@ -4,6 +4,53 @@ module.exports = {
     name : 'setcategory',
     run : async(client, message, args) => {
     	if (!message.guild.me.permissions.has("MANAGE_CHANNELS")) return message.channel.send(":x: Missing Permissions \`Manage Channels\`")
+    	if (!process.env.staff) {
+    	if (!message.member.permissions.has("ADMINISTRATOR")) return message.channel.send(":x: You don't have the required permission to run this command")
+    const id = args[0]
+    
+    if (!id) {
+    	const noarg = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎No Argument")
+    .setDescription("No argument for category ID was provided! Please provide the ID of the category where tickets will be created.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(noarg)
+    }
+    else {
+    	const cy = message.guild.channels.cache.get(id)
+    if (!cy) {
+    	const unknown = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎Unknown Argument")
+    .setDescription("The ID you provided was invalid. Please provide a valid ID.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(unknown)
+    }
+    else if  (cy.type !== "category") {
+    	const invalid = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("❌╎Invalid Argument")
+    .setDescription("The ID you provided was of a non-category channel. Please provide a category id.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(invalid)
+    	}
+    else {
+    	db.set("ticket.category", id)
+    const end = new MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setTitle("✅╎Category Change")
+    .setDescription("The tickets will be formed at **" + cy.name + "** from now on.")
+    .setFooter(message.author.username, message.author.displayAvatarURL({dynamic: true}));
+    message.channel.send(end)
+    }
+    
+    }
+    }
+    else {
     	if (!message.member.roles.cache.has(process.env.staff)) return message.channel.send(":x: You don't have the required permission to run this command")
     const id = args[0]
     
@@ -47,6 +94,7 @@ module.exports = {
     message.channel.send(end)
     }
     
+    }
     }
     
     }

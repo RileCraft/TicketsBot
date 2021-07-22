85a84555da0d44cbd42fa4a47d9113c6b47e28a7
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-07-22 10:24:45 +0530

    update lock-ticket-button.js

--- src/events/lock-ticket-button.js
+++ src/events/lock-ticket-button.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'clickButton',
 	execute(button, channel) {
 		if (button.id === "lock") {
-			button.reply.send(channel.id)
+			button.reply.send(button.channel.id)
 			}
 		},
 };

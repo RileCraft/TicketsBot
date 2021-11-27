63576b8c12d790aa8574718423b4de3a89b1ddf9
RileCraft <48475792+RileCraft@users.noreply.github.com>
2021-11-27 08:34:20 +0530

    update Config.js

--- Home/Storage/Vault/Config.js
+++ Home/Storage/Vault/Config.js
@@ -3,8 +3,8 @@ If multiple prefixes then ["!", "?"]
 If multiple devz then ["DEV ID", "DEV ID"]
 */
 module.exports = {
-	token: "NTU2MzM5MTg5ODQzNjIzOTQw.XIyBPw.now-ZIxVQ-MBorYaBj_l35Ye8R8",
-	prefix: "!",
-	dev: "427109850368049162",
-	staff: "764379457678475324" 
+	token: "Token",
+	prefix: "Prefix",
+	dev: "Owner ID",
+	staff: "Staff Role ID" 
 }
